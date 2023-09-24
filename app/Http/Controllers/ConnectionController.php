8781ba81fe091bed55e7f2b05722cac16335784c
unknown <yogesh@etelligens.in>
2023-09-24 21:26:27 +0530

    done latest changes

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use phpseclib3\Net\SSH2; 
+use phpseclib3\Net\SFTP; // Import the SFTP class
 class ConnectionController extends Controller
 {
     public function displayForm()
     {
-        
+
          return view('user\connect-form');
     }
 
@@ -18,25 +19,32 @@ class ConnectionController extends Controller
 
 
   
-        $host = $request->input('host');
+        $host =  $request->input('host');
         $username = $request->input('username');
         $password = $request->input('password');
         $port = $request->input('port');
-        $path = 'home/user/public_html/'; 
-
-        // Create an SSH2 instance and login
-        $ssh = new SSH2($host);
+        $domain='https://www.getgenuinereview.com/';
+        $path = 'public_html/maindir/';
+      
+       
+        $sftp = new SFTP($host, $port);
+      
+             
            try
            {
-              if ($ssh->login($username, $password)==false) {
+              if ($sftp->login($username, $password)==false) {
                 return back()->with('errors', 'Invalid Credentials');
                 
             }
             else
             {
-                $files = $ssh->exec("ls -la $path");
                 
-                 return View::make('user/user-file-list', ['files' => $files]);
+              
+                $sftp->chdir($path);
+    
+                $files = $sftp->nlist();               
+               
+                 return View::make('user/user-file-list', ['files' => $files,'domain'=>$domain,'path'=>$path]);
             }
            }
            catch(\Exception $e)
@@ -53,24 +61,6 @@ class ConnectionController extends Controller
     }
 
 
-    public function showuserFiles(Request $request)
-    {
-        
-        $host = $request->input('host');
-        $username = $request->input('username');
-        $password = $request->input('password');
-        $port = $request->input('port');
-
-      
-        $ssh = new SSH2($host, $port);
-        if (!$ssh->login($username, $password)) {
-            return redirect()->route('connect')->with('error', 'Failed to connect to the host.');
-        }       
-
+  
  
-        $directoryContents = $ssh->exec('ls -la /public');
-
-        
-        return view('file-list')->with('directoryContents', $directoryContents);
-    }
 }
--- resources/views/user/user-file-list.blade.php
+++ resources/views/user/user-file-list.blade.php
@@ -1,13 +1,21 @@
 @extends('pages.layouts.app')
 
 
+
     <div class="mt-5 container">
         <div class="row">
         <div class="col-sm-12">    
         <h3>Files and Folders</h3>
+        <div style="background: lightgrey;padding:4px" >
          @if(!empty($files))
-            {{$files}}
+         
+            @foreach($files as $file)
+            
+            <a style="font-weight:500px;color:blue;padding:2px" href="<?php echo $domain.'/maindir/'.$file ?>"><?php echo $file?></a><br/>
+            
+            @endforeach
          @else
+     </div>
            <h5 class="text-danger">Sorry No Files Available</h5>
          @endif 
        </div>
